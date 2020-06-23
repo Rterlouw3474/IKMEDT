48a61e155eda7df649f0ab88eab3ea46bb2df2d2
Sanne <SanneSVisser@gmail.com>
2020-06-23 11:28:49 +0200

    Setup + little tests

---
+++ js/main.js
@@ -0,0 +1,24 @@
+window.onload = () => {
+    const cursor = document.getElementById("js--cursor");
+    const box = document.getElementById("box");
+    const button = document.getElementById("button");
+    const toast = document.getElementById("toast");
+
+
+    button.addEventListener('click', () => {
+        var color = box.getAttribute('color');
+        if (color == "red") {
+            const text = toast.firstElementChild;
+            text.setAttribute('value', "It's green!");
+            toast.setAttribute('color', 'green');
+            box.setAttribute('color', 'green');
+        }
+        else if (color == "green") {
+            const text = toast.firstElementChild;
+            text.setAttribute('value', "It's red!");
+            toast.setAttribute('color', 'red');
+            box.setAttribute('color', 'red');
+        }
+    })
+
+}
