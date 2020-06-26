d391eada7d5a9c2013937225c600be8b4b270a16
thomasdecactus <thomaszijl@live.nl>
2020-06-26 16:12:31 +0200

    player model body rotation fix
    Now, the player model rotation in syn with the camera

--- js/player.js
+++ js/player.js
@@ -4,6 +4,8 @@ AFRAME.registerComponent('syn-player', {
       const playermodel = document.getElementById("js--player-model");
 
       console.log("move player model");
+      console.log(camera.getAttribute("rotation").y);
       playermodel.setAttribute("position", camera.getAttribute("position").x + " " + "0" + " " + camera.getAttribute("position").z);
+      playermodel.setAttribute("rotation", playermodel.getAttribute("rotation").x + " " + (camera.getAttribute("rotation").y+90) + " " + playermodel.getAttribute("rotation").z);
     },
 });
