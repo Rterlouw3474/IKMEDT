ef477ca6f219f561bc53a912092cb03da95136e0
Sanne <SanneSVisser@gmail.com>
2020-06-26 18:58:45 +0200

    Fix

--- js/aframe.js
+++ js/aframe.js
@@ -1,3 +1,39 @@
+AFRAME.registerComponent('toast', {
+    schema: {
+        message: { type: 'string', default: 'Hello world' }
+    },
+    init: function () {
+        this.el.addEventListener('click', (event) => {
+            Toast.showToast(this.data.message)
+        })
+    }
+});
+
+AFRAME.registerComponent("interactive-feedback", {
+    schema: {
+        light: { type: 'boolean', default: true }
+    },
+    init() {
+        const cursor = document.getElementById("js--cursor");
+        this.el.addEventListener("mouseenter", () => {
+            cursor.object3D.scale.set(1.5, 1.5, 1);
+            this.el.setAttribute("material", "emissive", "#222");
+            this.el.object3D.scale.set(1.05, 1.05, 1.05);
+        });
+        this.el.addEventListener("mouseleave", () => {
+            cursor.object3D.scale.set(1, 1, 1);
+            this.el.setAttribute("material", "emissive", "#000");
+            this.el.object3D.scale.set(1, 1, 1);
+        });
+    },
+});
+
+
+////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////Look-at component from aframe repo ////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+/**unpkg route werkte niet dus code copy-paste van https://github.com/supermedium/superframe/blob/master/components/look-at/index.js*/
+
 var debug = AFRAME.utils.debug;
 var coordinates = AFRAME.utils.coordinates;
 
