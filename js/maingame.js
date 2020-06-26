b7b91d1c5f2d40f9829d8339ae42ca39b19acf70
Sanne <SanneSVisser@gmail.com>
2020-06-26 13:43:18 +0200

    .

--- js/maingame.js
+++ js/maingame.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+window.onload = function () {
   const places = document.getElementsByClassName("direction");
   const camera = document.getElementById("js--camera");
   const restart = document.getElementsByClassName("js--restart");
@@ -8,14 +8,14 @@ window.onload = function() {
   let victory = true;
 
   for (let i = 0; i < restart.length; i++) {
-    restart[i].addEventListener("click", function(evt) {
+    restart[i].addEventListener("click", function (evt) {
       window.location.reload();
     });
   }
 
   for (let i = 0; i < start.length; i++) {
-    start[i].addEventListener("click", function(evt) {
-      informationPopup.setAttribute("visible",false);
+    start[i].addEventListener("click", function (evt) {
+      informationPopup.setAttribute("visible", false);
       for (let i = 0; i < places.length; i++) {
         places[i].classList.add('js--place')
       }
@@ -33,26 +33,36 @@ window.onload = function() {
         victory = false;
       })
     }
-
-
-
-    setTimeout(function() {
+    
+    setTimeout(function () {
       if (victory) {
         camera.innerHTML +=
-        '<a-entity><a-plane id="js--hold" class="js--pickup" color="green" position="-2 1 -3" width="1.5" height="1.5"><a-text position="0 0.3 0"><a-text id="menu-title" align="center" value="Gefeliciteerd!" width="5" color="black"></a-text></a-plane><a-plane position="-2 0.7 -3" color="green" geometry="height: 0.25" ><a-text wrap-count="20" value="Kijk naar de zon om nog een keer te spelen!" interactive-feedback color="black" text="align: center" width="1.5"></a-text></a-plane></a-entity>'
+          '<a-entity><a-plane id="js--hold" class="js--pickup" color="green" position="-2 1 -3" width="1.5" height="1.5"><a-text position="0 0.3 0"><a-text id="menu-title" align="center" value="Gefeliciteerd!" width="5" color="black"></a-text></a-plane><a-plane position="-2 0.7 -3" color="green" geometry="height: 0.25" ><a-text wrap-count="20" value="U gaat nu naar het eindscherm!" interactive-feedback color="black" text="align: center" width="1.5"></a-text></a-plane></a-entity>'
+
+          setTimeout(function () {
+            console.log("naar eindpagina");
+            navigateToEndscreen();
+          }, 2000);
+
       }
-      if (!victory){
+      if (!victory) {
         camera.innerHTML +=
-        '<a-entity><a-plane id="js--hold" class="js--pickup" color="red" position="-2 1 -3" width="1.5" height="1.5"><a-text position="0 0.3 0"><a-text id="menu-title" align="center" value="Helaas!" width="4" color="black"></a-text></a-plane><a-plane position="-2 0.7 -3" color="red" geometry="height: 0.25" ><a-text wrap-count="20" value="Kijk naar de zon om nog een keer te spelen!" interactive-feedback color="black" text="align: center" width="1.5"></a-text></a-plane></a-entity>'
-      }
+          '<a-entity><a-plane id="js--hold" class="js--pickup" color="red" position="-2 1 -3" width="1.5" height="1.5"><a-text position="0 0.3 0"><a-text id="menu-title" align="center" value="Helaas!" width="4" color="black"></a-text></a-plane><a-plane position="-2 0.7 -3" color="red" geometry="height: 0.25" ><a-text wrap-count="20" value="U gaat nu naar het eindscherm!" interactive-feedback color="black" text="align: center" width="1.5"></a-text></a-plane></a-entity>'
+
+          setTimeout(function () {
+            console.log("naar eindpagina");
+            navigateToEndscreen();
+          }, 2000);
+      
+        }
     }, 2000);
   }
 
   for (let i = 0; i < places.length; i++) {
-    places[i].addEventListener("click", function(evt) {
+    places[i].addEventListener("click", function (evt) {
       let att = document.createAttribute("animation");
       att.value = "property: position; easing: linear; dur: 2000; to: " + this.getAttribute('position').x + " 3 " +
-      this.getAttribute('position').z;
+        this.getAttribute('position').z;
       camera.setAttribute("animation", att.value);
       gameEnd();
     });
